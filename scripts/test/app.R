b8878dd3f7dae012b1a0f8a14c3bffa037e67d4a
Katie Murenbeeld <katiemurenbeeld@u.boisestate.edu>
2023-09-28 16:34:25 -0600

    updated user interface

--- scripts/test/app.R
+++ scripts/test/app.R
@@ -10,20 +10,11 @@ sheet_id <- "https://docs.google.com/spreadsheets/d/1EFbr-GahLJ0Hl01YYheOAjRny8G
 
 # Define UI for app that can generate a csv file from input options
 ui <- fluidPage(
+  DT::dataTableOutput("entries", width = 300), tags$hr(),
   titlePanel("Wildlife Conflict Data Entry"),
-  
-  fluidRow(
-    column(3,
-           textInput("article_title",
-           h3("Article Title"),
-           value = "")),
-    column(3,
-           textInput("publication",
-           h3("Publication"),
-           value = "")),
-    column(3,
-           selectInput("state",
-           h3("State"),
+  textInput("article_title", "Article Title", ""),
+  textInput("publication", "Publication", ""),
+  selectInput("state", "State" ,
            choices = list("AL" = 1, "AK" = 2, "AZ" = 3, "AR" = 4, "CA" = 5,
                           "CO" = 6, "CT" = 7, "DE" = 8, "DC" = 9, "FL" = 10,
                           "GA" = 11, "HI" = 12, "ID" = 13, "IL" = 14, "IN" = 15,
@@ -35,70 +26,58 @@ ui <- fluidPage(
                           "SC" = 41, "SD" = 42, "TN" = 43, "TX" = 44, "UT" = 45,
                           "VT" = 46, "VA" = 47, "WA" = 48, "WV" = 49, "WI" = 50,
                           "WY" = 51),
-           selected = 1)),
-   column(3,
-          textInput("city",
-                    h3("City"),
-                    value = "")),
-   column(3,
-          selectInput("species", 
-                      h3("Species"),
-                      choices = list("Grizzly Bear" = 1,
-                                     "Boar" = 2,
-                                     "Beaver" = 3,
-                                     "Buffalo" = 4,
-                                     "Mountain Lion" = 5,
-                                     "Wolf" = 6,
-                                     "Other" = 7),
-                      selected = 1)),
-    column(3,
-           dateInput("reviewer_1_date",
-                     h3("Reviewer 1 Date"),
-                     value = "2023-01-01")),
-    column(3,
-           selectInput("reviewer_1",
-                       h3("Reviewer 1"),
-                       choices = list("LP" = 1,
-                                      "SB" = 2,
-                                      "BW" = 3,
-                                      "KM" = 4,
-                                      "MW" = 5),
-                       selected = 1)),
-    column(3,
-           selectInput("type", 
-                       h3("Type"),
-                       choices = list("Human-Wildlife" = 1,
-                                      "Human-Human" = 2,
-                                      "Nature-Wildlife" = 3,
-                                      "Unstated Conflict" = 4),
-                       selected = 1)),
-    column(3,
-           selectInput("problem", 
-                       h3("Problem"),
-                       choices = list("Wildlife" = 1,
-                                      "People" = 2,
-                                      "Policy" = 3,
-                                      "Practicioner" = 4),
-                       selected = 1)),
-    column(3,
-           sliderInput("value",
-                       h3("Value Orientation"),
-                       min = 1, max = 7,
-                       value = 1)),
-   column(3,
-          textInput("comments",
-                    h3("Comments"),
-                    value = "")),
-   
-   actionButton("append", "Save Entry")
+           selected = 1),
+  textInput("city", "City", ""),
+  selectInput("species", "Species",
+              choices = list("Grizzly Bear" = 1,
+                             "Boar" = 2,
+                             "Beaver" = 3,
+                             "Buffalo" = 4,
+                             "Mountain Lion" = 5,
+                             "Wolf" = 6,
+                             "Other" = 7),
+              selected = 1),
+  dateInput("reviewer_1_date", "Reviewer 1 Date", "2023-09-01"),
+  selectInput("reviewer_1", "Reviewer 1",
+              choices = list("LP" = 1,
+                             "SB" = 2,
+                             "BW" = 3,
+                             "KM" = 4,
+                             "MW" = 5),
+              selected = 1),
+  selectInput("type", "Type",
+              choices = list("Human-Wildlife" = 1,
+                             "Human-Human" = 2,
+                             "Nature-Wildlife" = 3,
+                             "Unstated Conflict" = 4),
+              selected = 1),
+  selectInput("focus", "Focus",
+              choices = list("Wildlife" = 1,
+                             "People" = 2,
+                             "Policy" = 3,
+                             "Practicioner" = 4,
+                             "Ecosystem" = 5),
+              selected = 1),
+  sliderInput("value", "Value Orientation",
+              min = 1, max = 7,
+              value = 1),
+  textInput("comments", "Comments", ""),
+  actionButton("submit", "Submit")
   )
-)
 
 # Define server logic ----
 server <- function(input, output, session) {
   
-  # Save the data entered and append to a Google Sheet
-  output$append <- 
+  # Aggregate all form data
+  formData <- reactive({
+    data <- sapply(fields, function(x) input[[x]])
+    data
+  }) 
+  
+  # When the Submit button is clicks, save the form data
+  observeEvent(input$submit, {
+    saveData(formData())
+  })
   }
 
 
