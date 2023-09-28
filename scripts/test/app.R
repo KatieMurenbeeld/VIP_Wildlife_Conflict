4ab3488806e20c7fb6efd904434b4c5b5b83dafe
Katie Murenbeeld <katiemurenbeeld@u.boisestate.edu>
2023-09-28 14:12:23 -0600

    update state list, comment to add button widget

--- scripts/test/app.R
+++ scripts/test/app.R
@@ -19,10 +19,17 @@ ui <- fluidPage(
     column(3,
            selectInput("state",
            h3("State"),
-           choices = list("AL" = 1,
-                          "AK" = 2,
-                          "AZ" = 3,
-                          "CA" = 4),
+           choices = list("AL" = 1, "AK" = 2, "AZ" = 3, "AR" = 4, "CA" = 5,
+                          "CO" = 6, "CT" = 7, "DE" = 8, "DC" = 9, "FL" = 10,
+                          "GA" = 11, "HI" = 12, "ID" = 13, "IL" = 14, "IN" = 15,
+                          "IA" = 16, "KS" = 17, "LA" = 18, "ME" = 19, "MA" = 20,
+                          "MD" = 21, "MA" = 22, "MI" = 23, "MN" = 24, "MS" = 25,
+                          "MO" = 26, "MT" = 27, "NE" = 28, "NV" = 29, "NH" = 30,
+                          "NJ" = 31, "NM" = 32, "NY" = 33, "NC" = 34, "ND" = 35,
+                          "OH" = 36, "OK" = 37, "OR" = 38, "PA" = 39, "RI" = 40,
+                          "SC" = 41, "SD" = 42, "TN" = 43, "TX" = 44, "UT" = 45,
+                          "VT" = 46, "VA" = 47, "WA" = 48, "WV" = 49, "WI" = 50,
+                          "WY" = 51),
            selected = 1)),
    column(3,
           textInput("city",
@@ -57,7 +64,7 @@ ui <- fluidPage(
                        choices = list("Human-Wildlife" = 1,
                                       "Human-Human" = 2,
                                       "Nature-Wildlife" = 3,
-                                      "No Conflict" = 4),
+                                      "Unstated Conflict" = 4),
                        selected = 1)),
     column(3,
            selectInput("problem", 
@@ -70,7 +77,7 @@ ui <- fluidPage(
     column(3,
            sliderInput("value",
                        h3("Value Orientation"),
-                       min = 1, max = 5,
+                       min = 1, max = 7,
                        value = 1)),
    column(3,
           textInput("comments",
@@ -78,6 +85,8 @@ ui <- fluidPage(
                     value = ""))
   )
 )
+
+# Need a button widget to append to google sheet
 
 # Define server logic ----
 server <- function(input, output) {
