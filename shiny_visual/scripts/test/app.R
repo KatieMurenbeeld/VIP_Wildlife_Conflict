2634f73ab2d39d018cf36c49626c83d09fe4b95b
Katie Murenbeeld <katiemurenbeeld@u.boisestate.edu>
2023-11-01 21:49:52 -0600

    shiny app for plots and maps

---
+++ shiny_visual/scripts/test/app.R
@@ -0,0 +1,20 @@
+library(shiny)
+library(dplyr)
+library(ggplot2)
+library(googlesheets4)
+library(rsconnect)
+library(DT)
+library(lubridate)
+
+
+### Before running the app copy the following lines of code into the console
+# library(googlesheets4)
+# setwd('/Users/kathrynmurenbeeld/Analysis/VIP_Wildlife_Conflict/scripts/test/')
+# gs4_auth(email = "your@email.edu", cache = ".secrets")
+# Make sure to update your .gitignore to include .secrets and */.secrets
+# You will be taken to an authorization page, make sure to check the box that allows for editing
+###
+
+gs4_auth(cache = ".secrets", email = "katiemurenbeeld@boisestate.edu")
+
+sheet_id <- "https://docs.google.com/spreadsheets/d/1EFbr-GahLJ0Hl01YYheOAjRny8GFxHryFjnD5RNbQUY/edit#gid=0"
