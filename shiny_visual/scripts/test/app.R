e327df5007ea62cc7f34ca236267de194b33e44b
Katie Murenbeeld <katiemurenbeeld@u.boisestate.edu>
2023-11-02 09:31:45 -0600

    code to load google sheet

--- shiny_visual/scripts/test/app.R
+++ shiny_visual/scripts/test/app.R
@@ -2,6 +2,7 @@ library(shiny)
 library(dplyr)
 library(ggplot2)
 library(googlesheets4)
+library(googledrive)
 library(rsconnect)
 library(DT)
 library(lubridate)
@@ -17,4 +18,27 @@ library(lubridate)
 
 gs4_auth(cache = ".secrets", email = "katiemurenbeeld@boisestate.edu")
 
-sheet_id <- "https://docs.google.com/spreadsheets/d/1EFbr-GahLJ0Hl01YYheOAjRny8GFxHryFjnD5RNbQUY/edit#gid=0"
+folder_url <- "https://drive.google.com/drive/u/0/folders/1ob5sagTtT3svhc7ZKeemd9TiAq1_MsCL"
+folder <- drive_get(as_id(folder_url))
+
+gdrive_files <- drive_ls(folder)
+id <- gdrive_files[gdrive_files$name == "New Article Coding Framework", ]$id
+drive_download(id, path = "data/new_codes.csv", overwrite = TRUE)
+
+article_codes <- read.csv(file = 'data/new_codes.csv')
+
+#sheet_id <- "https://docs.google.com/spreadsheets/d/1EFbr-GahLJ0Hl01YYheOAjRny8GFxHryFjnD5RNbQUY/edit#gid=0"
+
+#data <- read_sheet(sheet_id)
+
+library(shiny)
+
+ui <- fluidPage(
+  
+)
+
+server <- function(input, output, session) {
+  
+}
+
+shinyApp(ui, server)
