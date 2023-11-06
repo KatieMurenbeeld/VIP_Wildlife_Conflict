ea48286d8129fd5aaa4a250b101065404fb0fdbc
Katie Murenbeeld <katiemurenbeeld@u.boisestate.edu>
2023-11-06 11:33:13 -0700

    viz script for VIP class

--- scripts/test/02_data_viz_app.R
+++ scripts/test/02_data_viz_app.R
@@ -1,13 +1,3 @@
-library(shiny)
-library(dplyr)
+library(tidyverse)
 library(ggplot2)
 
-# Define UI for app that can generate basic plots from Wildlife Conflict article coding
-ui <- fluidPage(
-  titlePanel("Wildlife Conflict Data Entry"), 
-  sidebarLayout(
-    sidebarPanel(
-      s
-    )
-  )
-)
